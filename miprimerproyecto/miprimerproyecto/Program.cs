318cc9f587274c87931db92908b9751d500cd2b8
Gabriela Lissette Amaya Garay <amayagabriela073@gmail.com>
2024-07-24 17:58:08 -0600

    suma de dos numeros pero con el tipo sbyte

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -10,18 +10,22 @@ namespace miprimerproyecto
     {
         static void Main(string[] args)
         {
-            //prioridad de los operadores aritmeticos.
-            //ejercicio obtener el promedio de una serie de numeros
-            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
-            int suma = 0;
-            foreach (int num in serie)  {
-                suma += num;
-            }
-            decimal prom = suma / serie.Length;
-            Console.WriteLine("la suma es: {0}, el promedio{1}", suma, prom);
+            Console.WriteLine(" Num1 : ");
+            int num1 = int.Parse(Console.ReadLine());
+          num1 =  sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Num 2 :");
+            int num2 = int.Parse(Console.ReadLine());
+            num2 = sbyte.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine(" la suma de {0} + {1} = {2} ", num1, num2, respuesta);
+            int suma = num1 + num2;
+
+
             //pausa.
             Console.ReadLine();
-
         }
+
     }
 }
