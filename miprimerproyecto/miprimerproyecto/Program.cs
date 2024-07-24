ae7ff46a64e1c9802fd2f0b60718b7122c5684a6
Gabriela Lissette Amaya Garay <amayagabriela073@gmail.com>
2024-07-24 16:36:26 -0600

    determinar el promedio de una serie de numeros

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -14,8 +14,7 @@ namespace miprimerproyecto
             //ejercicio obtener el promedio de una serie de numeros
             int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
             int suma = 0;
-            foreach (int num in serie)
-            {
+            foreach (int num in serie)  {
                 suma += num;
             }
             decimal prom = suma / serie.Length;
