b7505b210584a15c89dd2958e38fffdb12b83b60
Gabriela Lissette Amaya Garay <amayagabriela073@gmail.com>
2024-07-28 12:11:47 -0600

    prioridad de los operadores arimeticos

--- miprie¿merproyecto/miprie¿merproyecto/Program.cs
+++ miprie¿merproyecto/miprie¿merproyecto/Program.cs
@@ -10,31 +10,27 @@ namespace miprie_merproyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Num 1 : ");
-            sbyte num1 = sbyte.Parse(Console.ReadLine());
-
             Boolean comprobar = true;
             Console.WriteLine("El valor es: {0}", comprobar);
 
-            Console.WriteLine("Num 2: ");
-            sbyte num2 = sbyte.Parse(Console.ReadLine());
+            //prioridad de los operadores aritmeticos.
+            //se pone el sufijo m para la division con decimales.
 
-            Console.WriteLine("Nombre :");
+            decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1; //14.75 valor incorrecto; 11.5 → valor correcto
+
+            Console.WriteLine("La respuesta es: {0} ", respuesta) ;
+
+            Console.WriteLine("Nombre: ");
             string nombre = Console.ReadLine();
 
-            int suma = num1 + num2;
-           
-            Console.WriteLine(" La suma de {0} + {1} = {2}", num1, num2, suma);
-
-            Console.WriteLine(" Hola {0} bienvenido", nombre);
+            Console.WriteLine("Hola {0} bienvenido ", nombre);
 
             DateTime fechaActual = new DateTime();
             fechaActual = DateTime.Now;
-            Console.WriteLine("La fecha actual es : {0}", fechaActual);
-            //pausa
+            Console.WriteLine(" La fecha actual es : {0} ", fechaActual);
+
+            //pausa.
             Console.ReadLine();
-
-
         }
     }
 }
