8053219382b8ca24302c0899da361997ff58b39e
Gabriela Lissette Amaya Garay <amayagabriela073@gmail.com>
2024-07-28 11:10:36 -0600

    Tipo de dato en c#

--- miprie¿merproyecto/miprie¿merproyecto/Program.cs
+++ miprie¿merproyecto/miprie¿merproyecto/Program.cs
@@ -11,16 +11,30 @@ namespace miprie_merproyecto
         static void Main(string[] args)
         {
             Console.WriteLine("Num 1 : ");
-            int num1 = int.Parse(Console.ReadLine());
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
             Console.WriteLine("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
 
-            int respuesta = num1 + num2;
-            Console.WriteLine(" La suma de {0} + {1} = {2}", num1, num2, respuesta);
+            Console.WriteLine("Nombre :");
+            string nombre = Console.ReadLine();
 
+            int suma = num1 + num2;
+           
+            Console.WriteLine(" La suma de {0} + {1} = {2}", num1, num2, suma);
+
+            Console.WriteLine(" Hola {0} bienvenido", nombre);
+
+            DateTime fechaActual = new DateTime();
+            fechaActual = DateTime.Now;
+            Console.WriteLine("La fecha actual es : {0}", fechaActual);
             //pausa
             Console.ReadLine();
+
+
         }
     }
 }
