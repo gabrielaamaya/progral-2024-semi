da9ac4229be8d3c06f050e731f6d0373b35dd0d2
gabrielaamaya <amayagabriela073@gmail.com>
2024-08-20 10:18:46 -0600

    uso de funciones..

---
+++ mi primer proyecto/mi primer proyecto/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mi_primer_proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            //uso de funciones..
+            Console.WriteLine("La sum de 10+5={0}", suma());
+            Console.ReadLine();
+            {
+                static int suma();
+                int respuesta = 10 + 5;
+                return respuesta;
+            }
+        }
+    }
+}
