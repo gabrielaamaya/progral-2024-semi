b7b503b8c750c9788d957ae465af06037a456eb6
gabrielaamaya <amayagabriela073@gmail.com>
2024-08-20 13:30:22 -0600

    Uso de funciones con parametros

--- mi primer proyecto/mi primer proyecto/Form1.cs
+++ mi primer proyecto/mi primer proyecto/Form1.cs
@@ -13,14 +13,28 @@ namespace mi_primer_proyecto
     internal class Program
     {
         static void Main(string[] args){
-            //uso de funciones..
-            Console.WriteLine("La sum de 10+5={0}", suma());
+            //Uso de funciones...
+            Console.WriteLine("La suma de 10+5={0}", suma());
+
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
+
             Console.ReadLine();
-            {
-                static int suma();
-                int respuesta = 10 + 5;
-                return respuesta;
-            }
+        }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
+        }
+        static int suma(int a, int b)
+        {
+            int respuesta = a + b;
+            return respuesta;
         }
     }
 }
