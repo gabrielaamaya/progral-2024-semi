f3087871cbf9db81259f286558c6c6b68efb43d3
gabrielaamaya <amayagabriela073@gmail.com>
2024-09-03 20:10:16 -0600

    Resolucion de la tarea de los conversores utilizando programacion orientada a objetos 2

--- mi primer proyecto/mi primer proyecto/Program.cs
+++ mi primer proyecto/mi primer proyecto/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mi_primer_proyecto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -13,7 +14,48 @@ namespace miprimerproyecto
     {
         static void Main(string[] args)
         {
-            
+            ////Uso de funciones...
+            //ejercicio encontrar la media aritmetica y la desviacion tipica de una seria de numeros.
+            Conversores objConversor = new Conversores();
+            String continuar = "s";
+            while (continuar == "s")
+            {
+                Console.WriteLine("::.. MENU ..::");
+                Console.WriteLine("1. Monedas.");
+                Console.WriteLine("2. Longitud.");
+                Console.WriteLine("3. Masa.");
+                Console.WriteLine("4. Tiempo.");
+                Console.WriteLine("0. Salir");
+                Console.Write("Opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                if (opcion == 0)
+                {
+                    continuar = "n";
+                }
+                else
+                {
+                    Console.Clear();
+                    for (int i = 1; i < objConversor.etiquetas[opcion].Length; i++)
+                    {
+                        Console.WriteLine("{0}. {1}", i, objConversor.etiquetas[opcion][i]);
+                    }
+                    Console.Write("De: ");
+                    int de = int.Parse(Console.ReadLine());
+                    Console.Write("A: ");
+                    int a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Cantidad: ");
+                    double cantidad = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("{0} \n", objConversor.convertir(de, a, cantidad, opcion));
+                }
+
+
+            }
+
+            armonica = serie.Length / armonica;
+            return Math.Round(armonica, 2);
         }
     }
 }
